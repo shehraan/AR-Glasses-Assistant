a79879b7d9d1523b328adfc38cc91d8de1c060df
shehraan <shehraanhafiz@gmail.com>
2025-01-19 02:04:55 -0500

    Replaced real API Key with variable for public view

--- app/src/main/java/com/example/imagetest/MainActivity.java
+++ app/src/main/java/com/example/imagetest/MainActivity.java
@@ -143,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
             Request request = new Request.Builder()
                     .url("https://api.openai.com/v1/audio/transcriptions")
                     .post(requestBody)
-                    .addHeader("Authorization", "Bearer API")
+                    .addHeader("Authorization", "Bearer API-KEY")
                     .build();
 
             client.newCall(request).enqueue(new Callback() {
@@ -217,7 +217,7 @@ public class MainActivity extends AppCompatActivity {
             Request request = new Request.Builder()
                     .url("https://api.openai.com/v1/chat/completions")
                     .post(RequestBody.create(MediaType.parse("application/json"), jsonBody.toString()))
-                    .addHeader("Authorization", "Bearer API")
+                    .addHeader("Authorization", "Bearer API-KEY")
                     .addHeader("Content-Type", "application/json")
                     .build();
 
